0e93357596dfaa170d0aa28b5eefd15f374eafa1
kunanoa <81843512+kunanoa@users.noreply.github.com>
2021-04-04 10:12:43 +0000

    add ruby11_Ruby01.rb

---
+++ ruby11_Ruby01.rb
@@ -0,0 +1,12 @@
+# Ruby01 課題1、課題2 共通
+lists = [["“Branding Engineer”", "“Hello”", "“HR Tech”"], [1, 2, 3]]
+
+# Ruby01 課題1
+lists[0].each do |list|
+  puts list
+end
+
+# Ruby01 課題2
+lists[1].each do |list|
+  puts list
+end
