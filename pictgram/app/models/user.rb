0141f48748d7ab30d99c26a1bd255f4021521970
kunanoa <81843512+kunanoa@users.noreply.github.com>
2021-04-21 10:37:30 +0000

    rails05-after-kadai-fix01

--- pictgram/app/models/user.rb
+++ pictgram/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   
   has_secure_password
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]{8,32}\z/
